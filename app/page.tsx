afd91be1d644ecd9990924719a6fa09dc4c4d6c2
a-maystorov <alkin971@gmail.com>
2023-10-15 14:42:27 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>I WANT PROBLEMS!!</h1>
+      <p>ALWAYS!</p>
+    </main>
+  );
+}
