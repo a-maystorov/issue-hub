c2ba600740de9f77dfab9913136d40e7cf4173b1
a-maystorov <alkin971@gmail.com>
2023-10-16 18:39:10 +0200

    feat: add basic NavBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>I WANT PROBLEMS!!</h1>
-      <p>ALWAYS!</p>
-    </main>
-  );
+  return <h1>Main Content</h1>;
 }
