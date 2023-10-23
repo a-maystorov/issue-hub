634eb675540884e232cd6107d978075e7c587f7c
a-maystorov <alkin971@gmail.com>
2023-10-23 19:39:55 +0200

    feat: add new issue page with basic form elements

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 export default function IssuesPage() {
-  return <Button>New Issue</Button>;
+  return (
+    <Button>
+      <Link href="/issues/new">New issue</Link>
+    </Button>
+  );
 }
