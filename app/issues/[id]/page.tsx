dfcb118756d75ef9a144294b1fce9c7317083305
a-maystorov <alkin971@gmail.com>
2023-11-13 18:51:11 +0100

    refactor: simplify IssueDetails page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import ReactMarkdown from "react-markdown";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import Link from "next/link";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
@@ -21,25 +19,8 @@ export default async function IssueDetailsPage({ params }: Props) {
 
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <>
-        <Heading>{issue.title}</Heading>
-
-        <Flex gap="3" my="2">
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-
-        <Card className="prose" mt="4">
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
-      </>
-
-      <>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-        </Button>
-      </>
+      <IssueDetails issue={issue} />
+      <EditIssueButton issueId={issue.id} />
     </Grid>
   );
 }
