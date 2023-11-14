30459db9b98aa64ea5e28961f8bb59f533d2d6eb
a-maystorov <alkin971@gmail.com>
2023-11-14 18:44:06 +0100

    feat: add API for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,36 @@
+import { issueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const issue = prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  console.log(issue);
+
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    // TODO: Find out why passing `issue.id` is throwing an error.
+    where: { id: parseInt(params.id) },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
