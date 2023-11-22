621d1f81a4028d00428c52a52dfbc7824fc15ce0
a-maystorov <alkin971@gmail.com>
2023-11-22 18:17:37 +0100

    feat: add protected routes

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,15 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/auth/authOptions";
 
 interface Props {
   params: { id: string };
 }
 
 export default async function IssueDetailsPage({ params }: Props) {
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -24,12 +27,14 @@ export default async function IssueDetailsPage({ params }: Props) {
         <IssueDetails issue={issue} />
       </Box>
 
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </>
+      )}
     </Grid>
   );
 }
