6bb9bca6556463d4826869f6e4514fcec33acee4
a-maystorov <alkin971@gmail.com>
2023-12-03 14:11:58 +0100

    refactor: code cleanup

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,17 +7,16 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
-export default function AssigneeSelect({ issue }: { issue: Issue }) {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
+const useUsers = () =>
+  useQuery<User[]>({
     queryKey: ["users"],
     queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000,
     retry: 3,
   });
+
+export default function AssigneeSelect({ issue }: { issue: Issue }) {
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) {
     return <Skeleton />;
@@ -27,19 +26,21 @@ export default function AssigneeSelect({ issue }: { issue: Issue }) {
     return null;
   }
 
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId === "unassigned" ? null : userId,
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  };
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || "unassigned"}
-        onValueChange={(userId) => {
-          axios
-            .patch("/api/issues/" + issue.id, {
-              assignedToUserId: userId === "unassigned" ? null : userId,
-            })
-            .catch(() => {
-              toast.error("Changes could not be saved.");
-            });
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
 
@@ -58,7 +59,7 @@ export default function AssigneeSelect({ issue }: { issue: Issue }) {
         </Select.Content>
       </Select.Root>
 
-      <Toaster></Toaster>
+      <Toaster />
     </>
   );
 }
